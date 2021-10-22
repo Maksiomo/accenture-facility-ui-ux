6b798614fce067f77d78ed141bf8bff421cf1221
Maksiomo <maksim@golanov.ru>
2021-10-22 17:07:58 +0400

    set some ground

--- src/backend/Server.ts
+++ src/backend/Server.ts
@@ -4,8 +4,6 @@ import cors from "cors";
 const app = express();
 app.use(cors({ origin: "*" }));
 
-app.use("/dataProvider");
-
 app.listen(4444, "0.0.0.0", () => {
   console.log("Data provider server started! Port: " + 4444);
 });
