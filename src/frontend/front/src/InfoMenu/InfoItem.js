31b5b0b92cc85dc7b8a48aee7e98a49681a24db2
Zagarazhi <fukuroOwl@yandex.ru>
2021-10-23 13:00:22 +0400

    jumping info

--- src/frontend/front/src/InfoMenu/InfoItem.js
+++ src/frontend/front/src/InfoMenu/InfoItem.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 function InfoItem({info}){
     const color = 'rgb(255,' + (255 - (info.dangerTear/100*255)) + ','+ (255 - (info.dangerTear/100*255)) + ')';
-    const borderColor = '1px solid rgb(77,' + (255 - (info.dangerTear/100*255))*0.3 + ','+ (255 - (info.dangerTear/100*255))*0.3 + ')';
     return (
-        <li className='info-item' style={{backgroundColor: color, border: borderColor}}>
+        <li className='info-item' style={{backgroundColor: color}}>
             <span>
                 {info.description}
                 &nbsp;
