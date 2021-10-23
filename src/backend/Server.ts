318b2e4b0875b2d5cc0103a5d5f94e1a52b899da
Maksiomo <maksim@golanov.ru>
2021-10-23 15:36:12 +0400

    fix front

--- src/backend/Server.ts
+++ src/backend/Server.ts
@@ -4,13 +4,13 @@ import { DataProviderService } from "./DataProviderService";
 import { DataProviderController } from "./DataProviderController";
 
 const app = express();
-app.use(cors({ origin: "*" }));
+app.use(cors());
 
 const dataProviderService = new DataProviderService();
 const dataProvider = new DataProviderController(dataProviderService);
 
 app.use("/dataProvider", dataProvider.router);
 
-app.listen(5555, "0.0.0.0", () => {
-  console.log("Data provider server started! Port: " + 5555);
+app.listen(5555, () => {
+  console.log("Data provider server started! generic url: " + 5555);
 });
--- src/frontend/front/src/App.js
+++ src/frontend/front/src/App.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { func } from "prop-types";
 import React, { useEffect } from "react";
 import Context from "./Context";
 import HorizontalMenu from "./HorizontalMenu/HorizontalMenu";
@@ -7,27 +9,22 @@ import PlanOptionMenu from "./OptionsMenu/PlanOptionsMenu";
 import VerticalMenu from "./VerticalMenu/VerticalMenu";
 
 function App() {
-  const [items, setItems] = React.useState(
-    [
-      {title: 'Панель показателей', id: 0, picked: true},
-      {title: 'Визуализация плана', id: 1, picked: false},
-      {title: 'Проблемы', id: 2, picked: false},
-    ]);
-  const [options, setOptions] = React.useState(
-      [
-        {title: 'Сделать хорошо', id: 0, picked: false},
-        {title: 'Сделать плохо', id: 1, picked: false},
-      ]);
-  const [planOptions, setPlanOptions] = React.useState(
-    [
-      {title: 'Смотреть выгрузку по...', id: 0, picked: false},
-      {title: 'Сверить по дате', id: 1, picked: false},
-    ]);
-  const [infos, setInfos] = React.useState(
-    [
-      {elementName: 'Тест0', elementId: 0, legent: 0, dangerTier: 0},
-    ]);
-
+  const [items, setItems] = React.useState([
+    { title: "Панель показателей", id: 0, picked: true },
+    { title: "Визуализация плана", id: 1, picked: false },
+    { title: "Проблемы", id: 2, picked: false },
+  ]);
+  const [options, setOptions] = React.useState([
+    { title: "Сделать хорошо", id: 0, picked: false },
+    { title: "Сделать плохо", id: 1, picked: false },
+  ]);
+  const [planOptions, setPlanOptions] = React.useState([
+    { title: "Смотреть выгрузку по...", id: 0, picked: false },
+    { title: "Сверить по дате", id: 1, picked: false },
+  ]);
+  const [infos, setInfos] = React.useState([
+    { elementName: "Тест0", elementId: 0, legent: 0, dangerTier: 0 },
+  ]);
 
   const [loading, setLoading] = React.useState(true);
   const [panel, setPanel] = React.useState(true);
@@ -35,93 +32,103 @@ function App() {
   const [problem, setProblem] = React.useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:5555/DataProvider/analyzeStock').then((res) => {
-      console.log(res)
-    });
-  }, [])
+    sendRequest("http://127.0.0.1:5555/dataProvider/analyzeStock");
+  }, []);
 
-  function showPanel(){
+  async function sendRequest(url) {
+    return await axios.get(url);
+  }
+
+  function showPanel() {
     setPanel(true);
     setPlan(false);
     setProblem(false);
   }
 
-  function showPlan(){
+  function showPlan() {
     setPanel(false);
     setPlan(true);
     setProblem(false);
-    setPlanOptions(planOptions.map(
-      option => {
+    setPlanOptions(
+      planOptions.map((option) => {
         option.picked = false;
         return option;
-      }
-    ))
+      })
+    );
   }
 
-  function showProblem(){
+  function showProblem() {
     setPanel(false);
     setPlan(false);
     setProblem(true);
-    setOptions(options.map(
-      option => {
+    setOptions(
+      options.map((option) => {
         option.picked = false;
         return option;
-      }
-    ))
+      })
+    );
   }
 
-  function pickItem(id){
+  function pickItem(id) {
     setItems(
-      items.map(item =>{
-        item.picked = (item.id === id);
-        if(id === 0){ showPanel() };
-        if(id === 1){ showPlan() };
-        if(id === 2){ showProblem() };
+      items.map((item) => {
+        item.picked = item.id === id;
+        if (id === 0) {
+          showPanel();
+        }
+        if (id === 1) {
+          showPlan();
+        }
+        if (id === 2) {
+          showProblem();
+        }
         return item;
       })
-    )
+    );
   }
 
-  function pickOption(id){
+  function pickOption(id) {
     setOptions(
-      options.map(option =>{
-        option.picked = (option.id === id);
+      options.map((option) => {
+        option.picked = option.id === id;
         return option;
       })
-    )
+    );
   }
 
-  function pickPlanOption(id){
+  function pickPlanOption(id) {
     setPlanOptions(
-      planOptions.map(option =>{
-        option.picked = (option.id === id);
+      planOptions.map((option) => {
+        option.picked = option.id === id;
         return option;
       })
-    )
+    );
   }
 
   return (
-    <Context.Provider value={{pickItem, pickOption, pickPlanOption}}>
+    <Context.Provider value={{ pickItem, pickOption, pickPlanOption }}>
       <div>
-        <HorizontalMenu items={items}/>
+        <HorizontalMenu items={items} />
       </div>
-      <div className='wrapper'>
-        <div className='map'>
-          {loading && <Loader/>}
-        </div>
-        {problem ? <div className='side-menu'>
-          <div className='legent'>
-            <InfoMenu infos={infos}/>
+      <div className="wrapper">
+        <div className="map">{loading && <Loader />}</div>
+        {problem ? (
+          <div className="side-menu">
+            <div className="legent">
+              <InfoMenu infos={infos} />
+            </div>
+            <div className="vertical-menu-div">
+              <VerticalMenu options={options} />
+            </div>
           </div>
-          <div className='vertical-menu-div'>
-            <VerticalMenu options={options}/>
+        ) : null}
+        {plan ? (
+          <div className="side-menu">
+            <div className="vertical-menu-div">
+              <PlanOptionMenu options={planOptions} />
+            </div>
           </div>
-        </div> : null}
-        {plan ? <div className='side-menu'>
-          <div className='vertical-menu-div'>
-            <PlanOptionMenu options={planOptions}/>
-          </div>
-        </div> : null}
+        ) : null}
       </div>
     </Context.Provider>
   );
