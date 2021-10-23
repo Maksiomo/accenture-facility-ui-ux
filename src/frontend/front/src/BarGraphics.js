8ab952487d97d6a86b6494b0a85c51ce2e7533a6
Vasileva Vlada <vlada.3234.vasileva@gmail.com>
2021-10-24 02:41:36 +0400

    graph

--- src/frontend/front/src/BarGraphics.js
+++ src/frontend/front/src/BarGraphics.js
@@ -26,21 +26,23 @@ function BarGraphics({ data }) {
     height = 250;
   }
 
-
   return (
-    <div className='map' onClick={pickGraph.bind(null, data.id)}>
+    data.forEach((metric) => {
+      <div className='map' onClick={pickGraph.bind(null, data.id)}>
         <LineChart
         width= {width}
         height={height}
         data={data}
         margin={{ top: 5, right: 5, left: 5, bottom: 5 }}>
-        <Line type="monotone" dataKey={data.key1} />
+        <Line type="monotone" dataKey={data.paramMetrics.map((elem) => elem.data)} />
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-         <XAxis dataKey="name" tick={data.tick} /> 
+         <XAxis dataKey={data.paramMetrics.map((elem) => {return elem.metricName})} /> 
           <YAxis />
         <Tooltip />
       </LineChart>
     </div>
+    })
+    
   );
 }
 //XAxis dataKey="name" вместо name названия поля с датами
